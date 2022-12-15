5a086b7418f611e3d9c82cc6b0dec5f87b75ffe1
Eugene <jacky1suzuka@gmail.com>
2022-12-15 21:26:04 +0300

    check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car lada = new Car("", "Granta", 1.7, "жёлтый", 2015, "Россия");
+        Car audi = new Car("Audi", "", 3.0, "Чёрный", 2020, "Германия");
+        Car bmw = new Car("BMW", "Z8", -3.0, "Чёрный", 2021, "Германия");
+        Car kia = new Car("Kia", "Sportage", 2.4, "", 2018, "Южная Корея");
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "Оранжевый", 0, "");
+
+
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
     }
 }
